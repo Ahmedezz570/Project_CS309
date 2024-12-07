340a476541ad05edf30503fe90423db53ef35aec
Ahmedezz570 <ahmedez570@gmail.com>
2024-12-08 01:01:26 +0200

    Edit App.js and Change Title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Table from './Table';
-import { useState } from 'react';
-
 function App() {
-  const [showTable, setShowTable] = useState(false);
-  const handleClick = () => {
-    setShowTable(!showTable);
-  }
+ 
   return (
     <div className="App">
-    <button onClick={handleClick}>
-      {showTable ? "Hide" : "Show"} Table
-    </button>
-      {!showTable &&<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      }
-      {showTable && <Table/>}
+      <h1>Sport Store</h1>
     </div>
   );
 }

57ee9b865762265f5dca00335f7aab844bd0dc79
Ahmedezz570 <ahmedez570@gmail.com>
2024-12-08 01:12:16 +0200

    Add Components and Pages Folders and Create Some Files

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,43 @@
+
+import p5_img from '../Assets/th.jpg';
+import p6_img from '../Assets/th(1).jpg';
+import p7_img from '../Assets/th(2).jpg';
+import p8_img from '../Assets/th(3).jpg';
+
+
+let data_product = [
+  
+  {
+    id:5,
+    name:"Lamine YAmal",
+    image: p5_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+ 
+  {
+    id:5,
+    name:"Lamine YAmal",
+    image: p6_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+ 
+  {
+    id:5,
+    name:"Lamine YAmal",
+    image: p7_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+  
+  {
+    id:5,
+    name:"Lamine YAmal",
+    image: p8_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;

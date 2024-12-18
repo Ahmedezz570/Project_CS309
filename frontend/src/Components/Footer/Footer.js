d0bdbcf58fe3e5aa3374a103c0b9f2db1b9b32bb
bavly <bavlymomtaz10@gmail.com>
2024-12-18 14:05:06 +0200

    finish the home page

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import './Footer.css';
+import cover from '../../Components/Assets/EPL Player Of The Year.jpg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer >
+
+      <div className='any'>
+        <div className="fir_half flex">
+          <h2> Striker</h2>
+          <form action="" style={{ marginTop: '22px' }}>
+            <p> Sign up for more info</p>
+            <input type="text" name='userEmail' placeholder='Enter your name' />
+            <input type="email" name='userEmail' placeholder='Enter your email' />
+
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+      <div className="sec-half container flex">
+        <div className='find'>
+          <h4> Find Your way </h4>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Products</a>
+          </li>
+          <li>
+            <a href="">Special</a>
+          </li>
+          <li>
+            <a href=""> Contact us</a>
+          </li>
+        </div>
+        <div className="branch">
+          <h4> Branchs</h4>
+          <li> Cairo </li>
+          <li> Giza </li>
+          <li> Alexandria </li>
+          <li> Aswan </li>
+        </div>
+      <img src={cover} alt="" />
+      </div>
+    </footer>
   )
 }
 
--- frontend/src/Pages/home/Shop.js
+++ frontend/src/Pages/home/Shop.js
@@ -3,7 +3,19 @@ import '../CSS/Shop.css';
 import Navbar from '../../Components/NavBar/Navbar';
 import Sell from './Sell'
 import '../Tranning'
+import shoe from '../../Components/Assets/—Pngtree—casual shoes_5640199.png'
+import brand from '../../Components/Assets/brand-36-1.png'
+import brand1 from '../../Components/Assets/brand-36-2.png'
+import brand2 from '../../Components/Assets/brand-36-3.png'
+import brand3 from '../../Components/Assets/brand-36-4.png'
+import brand4 from '../../Components/Assets/brand-36-5.png'
+import brand5 from '../../Components/Assets/brand-36-6.png'
+import brand6 from '../../Components/Assets/brand-36-7.png'
+import Footer from '../../Components/Footer/Footer';
 const Shop = () => {
+
+  // const date = new Date()
+  // prompt(date.toLocaleTimeString())
   return (
     <div>
       <div className="nav-sec">
@@ -11,42 +23,83 @@ const Shop = () => {
       </div>
       <div className="container">
         <section className='logos'>
-        <div className='logo'>
-        <i className="fa-regular fa-paper-plane"></i>
-        <div>
-        <h3>Free Worlwide Shipping</h3>
-        <p> Lorem Ipsum Is Simply Dummy Text
-        Lorem Ipsum Lorem ipsum dolor, sit amet  </p> 
-        </div>
-        </div>
-        <div className='logo'>
-        <i className="fa-regular fa-paper-plane"></i>
-        <div>
-        <h3>Free Worlwide Shipping</h3>
-        <p> Lorem Ipsum Is Simply Dummy Text
-        Lorem Ipsum Lorem ipsum dolor, sit amet  </p> 
-        </div>
-        </div>
-        <div className='logo'>
-        <i className="fa-regular fa-paper-plane"></i>
-        <div>
-        <h3>Free Worlwide Shipping</h3>
-        <p> Lorem Ipsum Is Simply Dummy Text
-        Lorem Ipsum Lorem ipsum dolor, sit amet  </p> 
-        </div>
-        </div>
+          <div className='logo'>
+            <i className="fa-regular fa-paper-plane"></i>
+            <div>
+              <h3>Free Worlwide Shipping</h3>
+              <p> Lorem Ipsum Is Simply Dummy Text
+                Lorem Ipsum Lorem ipsum dolor, sit amet  </p>
+            </div>
+          </div>
+          <div className='logo'>
+            <i className="fa-regular fa-paper-plane"></i>
+            <div>
+              <h3>Free Worlwide Shipping</h3>
+              <p> Lorem Ipsum Is Simply Dummy Text
+                Lorem Ipsum Lorem ipsum dolor, sit amet  </p>
+            </div>
+          </div>
+          <div className='logo'>
+            <i className="fa-regular fa-paper-plane"></i>
+            <div>
+              <h3>Free Worlwide Shipping</h3>
+              <p> Lorem Ipsum Is Simply Dummy Text
+                Lorem Ipsum Lorem ipsum dolor, sit amet  </p>
+            </div>
+          </div>
 
         </section>
-      
-        <Sell/>
+
+        <Sell />
       </div>
-        <section className='img-sec'>
-          <div className="content">
-        <p> flat 30% discount</p>
-        <h2> football equipment</h2>
-        <a href="../Tranning.js"> view more</a>
+      <section className='img-sec'>
+
+        <div className="content">
+          <p> flat 30% discount</p>
+          <h2> football equipment</h2>
+          <a href="../Tranning.js"> view more</a>
+        </div>
+      </section>
+      <section className='special_pro container'>
+        <div className='contain'>
+          <div className='spec-text'>
+            <p className='abs-p'> Deal</p>
+            <h3> deal of the week </h3>
+            <p style={{ margin: "8% 0", fontfamily: " cursive" }}>Hurry up! Products discounts up to 70%. </p>
+            <a href=""> + view all avaliable products </a>
           </div>
-        </section>
+          <div className='products'>
+            <div className="sm-card">
+              <img src={shoe} alt=" no shoe " />
+              <p className='name'> sport shoe </p>
+              <p className='pricee'>$30.00</p>
+              <del> $50.00</del>
+            </div>
+            <div className="sm-card" style={{borderLeft:"1px solid" , borderRight:"1px solid"}}>
+              <img src={shoe} alt=" no shoe " />
+              <p className='name'> sport shoe </p>
+              <p className='pricee'>$30.00</p>
+              <del> $50.00</del>
+            </div>
+            <div className="sm-card">
+              <img src={shoe} alt=" no shoe " />
+              <p className='name'> sport shoe </p>
+              <p className='pricee'>$30.00</p>
+              <del> $50.00</del>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='final container'>
+      <img src={brand} alt="" />
+      <img src={brand1} alt="" />
+      <img src={brand2} alt="" />
+      <img src={brand3} alt="" />
+      <img src={brand4} alt="" />
+      <img src={brand5} alt="" />
+      <img src={brand6} alt="" />
+      </section>
+      <Footer/>
     </div>
 
 
